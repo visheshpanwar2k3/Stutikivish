797f7cd484565cd1102f54d3d26aba4bdae55ed7
Vishesh Panwar <85211450+visheshpanwar2k3@users.noreply.github.com>
2023-12-16 16:21:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 1,
+        slideShadows: false, 
+    },
+    pagination:{
+        el: ".swiper pagination",
+    },
+});
+
+
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
